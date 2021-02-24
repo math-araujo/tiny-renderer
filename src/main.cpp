af6e2724871e4d4ac09b789a94b774d297783c33
M-A-rs <araujo_m_@hotmail.com>
2021-02-23 22:25:46 -0300

    Add first TGAImage render

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
+    const TGAColor white{255, 255, 255, 255};
+    const TGAColor red{255, 0, 0, 255};
+    
+    TGAImage image{100, 100, TGAImage::RGB};
+    image.set(52, 41, red);
+    image.flip_vertically(); // set origin to left bottom corner
+    image.write_tga_file("output.tga");
+
     return 0;
 }
